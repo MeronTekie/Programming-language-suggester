608a26c14a7043c5f37229064ce19a5152d5f212
Meron G.Tekie <mtekiesd210309@gmail.com>
2022-03-25 11:34:50 -0700

    Add styles to the webpage

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,28 @@
 $(document).ready(function () {
   $("form#suggester").submit(function (event) {
     event.preventDefault();
-    console.log("Thank You!");
 
     const age = parseInt($("input#age").val());
     const computerPart = parseInt($("#computer").val());
     const activity = parseInt($("select#activity").val());
     const favoriteFood = parseInt($("select#fav-food").val());
     const favoriteMusic = parseInt($("select#age").val());
-    console.log(age);
-    console.log(computerPart);
-    if (age >= "21" && age <= "45" && computerPart == "2") {
+
+    if (age <= "25" && activity <= "2" && computerPart == "2") {
       $("#output").text("We highly recommend you to study JavaScript!");
+    } else if (
+      age >= "26" &&
+      age < "40" &&
+      activity == "3" &&
+      computerPart == "3"
+    ) {
+      $("#output").text("Jave suits perfectly for you!");
+    } else if (age >= "40" && activity >= "1" && computerPart >= "1") {
+      $("#output").text("Python may be  the most suitable language for you!");
+    } else {
+      $("#output").text(
+        "You better get enrolled in to any langauge before its too late"
+      );
     }
   });
 });
